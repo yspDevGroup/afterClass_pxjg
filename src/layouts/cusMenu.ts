980520d6803d6a255e7d75e6c377db45cff9bdf4
SissleLynly <861271413@qq.com>
2021-08-25 08:45:12 +0800

    增加基础信息管理菜单

--- src/layouts/cusMenu.ts
+++ src/layouts/cusMenu.ts
@@ -2,8 +2,8 @@
  * @description:
  * @author: Sissle Lynn
  * @Date: 2021-08-24 17:21:12
- * @LastEditTime: 2021-08-24 19:29:00
- * @LastEditors: wsl
+ * @LastEditTime: 2021-08-25 08:43:53
+ * @LastEditors: Sissle Lynn
  */
 export default [
   {
@@ -11,11 +11,11 @@ export default [
     name: '首页'
   },
   {
-    path: '/schoolManagement',
-    name: '学校管理'
+    path: '/infoMaintenance',
+    name: '基础信息管理'
   },
   {
-    path: '/infoMaintenance',
-    name: '信息维护'
+    path: '/schoolManagement',
+    name: '学校管理'
   }
 ];
