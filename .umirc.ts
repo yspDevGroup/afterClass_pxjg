4add6fe0d9df76dcd5bcc7b98090dc507e8c4e2c
zhupengliang <zpl_dongxie@sina.com>
2021-10-10 19:22:02 +0800

    开启gzip代码压缩

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'umi';
+import CompressionWebpackPlugin from 'compression-webpack-plugin';
 import theme from './theme';
+
+const prodGzipList = ['js', 'css'];
 
 export default defineConfig({
   mock: false,
@@ -24,6 +27,21 @@ export default defineConfig({
     loading: '@/components/Loading'
   },
   theme,
+  chainWebpack: (config) => {
+    // 以下为gzip配置内容
+    // if (process.env.NODE_ENV === 'production') {
+      // 生产模式开启
+      config.plugin('compression-webpack-plugin').use(
+        new CompressionWebpackPlugin({
+          // filename: 文件名称，这里我们不设置，让它保持和未压缩的文件同一个名称
+          algorithm: 'gzip', // 指定生成gzip格式
+          test: new RegExp('\\.(' + prodGzipList.join('|') + ')$'), // 匹配哪些格式文件需要压缩
+          threshold: 10240, //对超过10k的数据进行压缩
+          minRatio: 0.6, // 压缩比例，值为0 ~ 1
+        }),
+      );
+    // }
+  },
   proxy: {
     '/api': {
       target: 'http://api.test.xianyunshipei.com',
