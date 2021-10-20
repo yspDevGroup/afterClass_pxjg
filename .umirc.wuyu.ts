8a66c6451abd266609cbb3ccadfdec7501814db5
朱鹏亮 <zpl_dongxie@sina.com>
2021-10-20 18:12:44 +0800

    增加五育编译配置

--- .umirc.wuyu.ts
+++ .umirc.wuyu.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-09-24 16:43:37
- * @LastEditTime: 2021-10-19 17:51:49
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-10-20 17:11:57
+ * @LastEditors: zpl
  * @Description: In User Settings Edit
  * @FilePath: \afterClass_pxjg\.umirc.ts
  */
@@ -23,12 +23,12 @@ export default defineConfig({
     ENV_title: '课后帮',
     ENV_subTitle: '课后服务平台',
     ENV_copyRight: '2021 版权所有：陕西五育汇智信息技术有限公司',
-    ENV_host: 'http://afterclassPxjg.prod.xianyunshipei.com',
-    ENV_backUrl: 'http://api.prod.xianyunshipei.com',
-    ssoHost: 'http://sso.prod.xianyunshipei.com',
+    ENV_host: 'http://afterclassPxjg.wuyu.imzhiliao.com',
+    ENV_backUrl: 'http://api.wuyu.imzhiliao.com',
+    ssoHost: 'http://sso.wuyu.imzhiliao.com',
     authType: 'wechat',
-    clientId: 'wwa3e7ccaca504ab3d',
-    clientSecret: 'z-DxOlOgLADZ_4MtEHIoYX0WrUFHwoXVDbOyzPiVtF4'
+    clientId: 'ww47296cadbd4de507',
+    clientSecret: 'Do9LvJl3Xnf8Tj0rt9iy-wvcT-shBvdzsQZqp4IKkJc'
   },
   links: [{ rel: 'icon', href: './title.png' }],
   dynamicImport: {
