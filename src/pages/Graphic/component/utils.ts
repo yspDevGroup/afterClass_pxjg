b3fc2dc16143a7f504388d612dbc203129dd51cf
WeiShiLong <979803984@qq.com>
2021-11-12 17:06:48 +0800

    数据大屏退款情况样式调整

--- src/pages/Graphic/component/utils.ts
+++ src/pages/Graphic/component/utils.ts
@@ -20,6 +20,8 @@ export const columnConfig: any = {
   isStack: true,
   color: ['#ff6e76', '#fddd60'],
   legend: {
+    layout: 'horizontal',
+    position: 'top-right',
     itemName: {
       style: {
         fill: '#fff'
@@ -32,7 +34,7 @@ export const columnConfig: any = {
       autoRotate: false,
       style: {
         fill: 'white',
-        fontSize: 14,
+        fontSize: 14
       }
     }
   },
@@ -45,12 +47,12 @@ export const columnConfig: any = {
   maxColumnWidth: 30,
   meta: {
     type: { alias: '课程名称' },
-    value: { alias: '金额' },
+    value: { alias: '金额' }
   },
   label: {
     position: 'middle',
-    style: { fill: '#fff' },
-  },
+    style: { fill: '#fff' }
+  }
 };
 export const pieConfig: any = {
   appendPadding: 20,
@@ -64,7 +66,7 @@ export const pieConfig: any = {
   pieStyle: {
     stroke: '#fff',
     lineWidth: 0,
-    strokeOpacity: 0,
+    strokeOpacity: 0
   },
   label: {
     type: 'spider',
@@ -72,14 +74,14 @@ export const pieConfig: any = {
     content: '{name}',
     style: {
       fill: 'white',
-      fontSize: 15,
+      fontSize: 15
     }
   },
   interactions: [{ type: 'element-selected' }, { type: 'element-active' }],
   statistic: {
     title: false,
-    content: false,
-  },
+    content: false
+  }
 };
 export const barConfig: any = {
   yField: 'school',
@@ -90,15 +92,15 @@ export const barConfig: any = {
       autoRotate: false,
       style: {
         fill: 'white',
-        fontSize: 14,
+        fontSize: 14
       }
     }
   },
   maxBarWidth: 30,
   meta: {
     school: { alias: '学校名称' },
-    value: { alias: '报名人数' },
-  },
+    value: { alias: '报名人数' }
+  }
 };
 export const defaultData: any = {
   serviceNum: [],
@@ -118,85 +120,106 @@ export const getTerm = () => {
     return {
       XN: `${year - 1}-${year}`,
       XQ: '第二学期'
-    }
+    };
   } else {
     return {
       XN: `${year}-${year + 1}`,
       XQ: '第一学期'
-    }
+    };
   }
-}
+};
 export const mock: any = {
-  serviceNum: [{
-    title: '教师总数',
-    num: '45236'
-  },
-  {
-    title: '学生总数',
-    num: '45736'
-  },
-  {
-    title: '收款总额',
-    num: '70789'
-  },
-  {
-    title: '退款总额',
-    num: '4455'
-  }],
-  courseCollect: [{
-    type: '艺术类',
-    value: 34
-  }, {
-    type: '人文社科类',
-    value: 54
-  }, {
-    type: '体育类',
-    value: 23
-  }, {
-    type: '自然科学类',
-    value: 31
-  }, {
-    type: '社会实践类',
-    value: 13
-  }, {
-    type: '托管服务',
-    value: 37
-  }, {
-    type: '其他',
-    value: 12
-  }],
-  checkOut: [{
-    icon: should,
-    title: '应到人数',
-    num: 2100
-  }, {
-    icon: real,
-    title: '实到人数',
-    num: 1842
-  }, {
-    icon: leave,
-    title: '请假人数',
-    num: 258
-  }],
-  numCollect: [{
-    title: '课程总数',
-    num: 21
-  }, {
-    title: '课程班总数',
-    num: 28
-  }, {
-    title: '学校教师数',
-    num: 123
-  }, {
-    title: '合作机构数',
-    num: 234
-  }, {
-    title: '合作课程数',
-    num: 45
-  }, {
-    title: '机构教师数',
-    num: 23
-  }],
+  serviceNum: [
+    {
+      title: '教师总数',
+      num: '45236'
+    },
+    {
+      title: '学生总数',
+      num: '45736'
+    },
+    {
+      title: '收款总额',
+      num: '70789'
+    },
+    {
+      title: '退款总额',
+      num: '4455'
+    }
+  ],
+  courseCollect: [
+    {
+      type: '艺术类',
+      value: 34
+    },
+    {
+      type: '人文社科类',
+      value: 54
+    },
+    {
+      type: '体育类',
+      value: 23
+    },
+    {
+      type: '自然科学类',
+      value: 31
+    },
+    {
+      type: '社会实践类',
+      value: 13
+    },
+    {
+      type: '托管服务',
+      value: 37
+    },
+    {
+      type: '其他',
+      value: 12
+    }
+  ],
+  checkOut: [
+    {
+      icon: should,
+      title: '应到人数',
+      num: 2100
+    },
+    {
+      icon: real,
+      title: '实到人数',
+      num: 1842
+    },
+    {
+      icon: leave,
+      title: '请假人数',
+      num: 258
+    }
+  ],
+  numCollect: [
+    {
+      title: '课程总数',
+      num: 21
+    },
+    {
+      title: '课程班总数',
+      num: 28
+    },
+    {
+      title: '学校教师数',
+      num: 123
+    },
+    {
+      title: '合作机构数',
+      num: 234
+    },
+    {
+      title: '合作课程数',
+      num: 45
+    },
+    {
+      title: '机构教师数',
+      num: 23
+    }
+  ],
   schoolNum: [
     '西安市高新区第七高级中学',
     '西安高新区第五高级中学',
@@ -213,121 +236,125 @@ export const mock: any = {
     {
       type: '收款情况',
       school: '西安市高新区第七高级中学',
-      value: 35,
+      value: 35
     },
     {
       type: '收款情况',
       school: '西安高新区第五高级中学',
-      value: 39,
+      value: 39
     },
     {
       type: '收款情况',
       school: '西安高新区第四高级中学',
-      value: 47,
+      value: 47
     },
     {
       type: '收款情况',
       school: '西安高新一中分校(高新区第三学校)',
-      value: 42,
+      value: 42
     },
     {
       type: '收款情况',
       school: '西安市鄠邑区秦风初级中学',
-      value: 34,
+      value: 34
     },
     {
       type: '收款情况',
       school: '西北大学附属中学',
-      value: 28,
+      value: 28
     },
     {
       type: '退款情况',
       school: '西安市高新区第七高级中学',
-      value: 23,
+      value: 23
     },
     {
       type: '退款情况',
       school: '西安高新区第五高级中学',
-      value: 26,
+      value: 26
     },
     {
       type: '退款情况',
       school: '西安高新区第四高级中学',
-      value: 38,
+      value: 38
     },
     {
       type: '退款情况',
       school: '西安高新一中分校(高新区第三学校)',
-      value: 17,
+      value: 17
     },
     {
       type: '退款情况',
       school: '西安市鄠邑区秦风初级中学',
-      value: 29,
+      value: 29
     },
     {
       type: '退款情况',
       school: '西北大学附属中学',
-      value: 28,
-    },
+      value: 28
+    }
   ],
   enrollNum: [
     {
       school: '西安市高新区第七高级中学',
-      value: 35,
+      value: 35
     },
     {
       school: '西安高新区第五高级中学',
-      value: 39,
+      value: 39
     },
     {
       school: '西安高新区第四高级中学',
-      value: 47,
+      value: 47
     },
     {
       school: '西安高新一中分校(高新区第三学校)',
-      value: 42,
+      value: 42
     },
     {
       school: '西安市高新区第三高级中学',
-      value: 28,
+      value: 28
     },
     {
       school: '西安市鄠邑区秦风初级中学',
-      value: 34,
+      value: 34
     },
     {
       school: '西北大学附属中学',
-      value: 28,
+      value: 28
     },
     {
       school: '西安市高新区第七高级中学',
-      value: 23,
+      value: 23
     },
     {
       school: '西安市高新区第六高级中学',
-      value: 34,
+      value: 34
     },
     {
       school: '西安市高新区第八高级中学',
-      value: 21,
+      value: 21
     },
     {
       school: '西安市高新区第九高级中学',
-      value: 23,
-    },
+      value: 23
+    }
   ],
-  checkOutTeacher: [{
-    icon: should,
-    title: '应到人数',
-    num: 2100
-  }, {
-    icon: real,
-    title: '实到人数',
-    num: 1842
-  }, {
-    icon: leave,
-    title: '请假人数',
-    num: 258
-  }]
+  checkOutTeacher: [
+    {
+      icon: should,
+      title: '应到人数',
+      num: 2100
+    },
+    {
+      icon: real,
+      title: '实到人数',
+      num: 1842
+    },
+    {
+      icon: leave,
+      title: '请假人数',
+      num: 258
+    }
+  ]
 };
