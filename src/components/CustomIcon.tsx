27d51953bc1d89d56034bbf023a8f962db8a7d23
‘WuXing’ <1205118129@qq.com>
2021-11-25 16:24:35 +0800

    无图片默认样式

--- src/components/CustomIcon.tsx
+++ src/components/CustomIcon.tsx
@@ -2,7 +2,7 @@ import { createFromIconfontCN } from '@ant-design/icons';
 
 const IconFont = createFromIconfontCN({
   scriptUrl: [
-    '//at.alicdn.com/t/font_2608150_sdswopk0a7.js',
+    '//at.alicdn.com/t/font_2608150_h6q0ri9u45g.js',
     '//at.alicdn.com/t/font_2608150_mg3cbf72k8.js',
     '//at.alicdn.com/t/font_2600907_5681fmwfnyw.js'
   ]
