071401639b84b0a42465c60084b021c1e0b9229b
zhupengliang <zpl_dongxie@sina.com>
2021-12-17 19:02:49 +0800

    更新版本号

--- src/components/Version.tsx
+++ src/components/Version.tsx
@@ -2,14 +2,14 @@
  * @description: 版本号显示组件
  * @author: zpl
  * @Date: 2021-11-16 08:55:11
- * @LastEditTime: 2021-12-14 11:25:51
+ * @LastEditTime: 2021-12-17 19:02:39
  * @LastEditors: zpl
  */
 import React from 'react';
 import type { CSSProperties } from 'react';
 
 const Version = ({ style }: { style?: CSSProperties }) => {
-  return <div style={style}>版本号：V2.2.1213</div>;
+  return <div style={style}>版本号：V2.2.1217</div>;
 };
 
 export default Version;
