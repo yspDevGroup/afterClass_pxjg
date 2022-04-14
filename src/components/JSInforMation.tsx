538a69944c1e8c7c98734109fbcd4cc72ff05d2e
wuzhan <1076795708@qq.com>
2022-04-14 09:38:59 +0800

    fix: 培训机构没有绑定账号提示修改关闭方式

--- src/components/JSInforMation.tsx
+++ src/components/JSInforMation.tsx
@@ -2,7 +2,7 @@
  * @description:
  * @author: Wu Zhan
  * @Date: 2022-04-12 15:36:27
- * @LastEditTime: 2022-04-12 16:07:19
+ * @LastEditTime: 2022-04-13 18:49:00
  * @LastEditors: Wu Zhan
  */
 import { Modal } from 'antd';
@@ -22,6 +22,7 @@ export const JSInforMation = (currentUser: UserInfo | null | undefined) => {
       title: '审批需要绑定教师 请前往认证平台',
       width: '450px',
       okText: '去设置',
+      maskClosable: true,
       onOk: () => {
         window.open('http://platform.test.xianyunshipei.com/oauth2/Password');
       }
